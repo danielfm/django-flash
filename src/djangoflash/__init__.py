d2d8b6ea7e3901eaeb650874bcc6cd663c06bad5
danielfm <daniel.tritone@gmail.com>
2009-08-21 12:57:25 -0300

    Added a new function that can be used to check if an already installed version of Django-Flash (e.g. one that don't have a 'setup.py' file) works as expected.

--- src/djangoflash/__init__.py
+++ src/djangoflash/__init__.py
@@ -12,3 +12,18 @@
 
 __version__ = '1.6.1'
 
+__author__  = 'Daniel Fernandes Martins'
+__email__   = 'daniel@destaquenet.com'
+
+
+def run_tests(verbosity=1):
+    """Runs the tests. This function is useful when you want to check if an
+    already installed version of Django-Flash (e.g. one that don't have a
+    ``setup.py`` file) works as expected. Example::
+    
+        $ python -c "import djangoflash; djangoflash.run_tests();"
+    """
+    from djangoflash.tests import suite
+    import unittest
+    runner = unittest.TextTestRunner(verbosity=verbosity)
+    unittest.main(module=suite, testRunner=runner)
