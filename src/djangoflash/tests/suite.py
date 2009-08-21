2e925999583bebcec4581c0e842c1048e8a900a9
danielfm <daniel.tritone@gmail.com>
2009-08-21 12:54:53 -0300

    Sanity check.

--- src/djangoflash/tests/suite.py
+++ src/djangoflash/tests/suite.py
@@ -10,17 +10,23 @@ from django.core.management import setup_environ
 import djangoflash.tests.testproj.settings as project_settings
 sys.path.insert(0, setup_environ(project_settings))
 
-# Bootstraps Django test environment
-from django.test.utils import setup_test_environment
-from django.db import connection
-setup_test_environment()
-connection.creation.create_test_db()
-
 # Imports unit tests
 from context_processors import *
 from decorators import *
 from models import *
 from storage import *
 
-# Imports integration tests
-from testproj.app.tests import *
+try:
+    # Required dependency
+    import sqlite3
+
+    # Bootstraps integration environment
+    import django.test.utils as test_utils
+    from django.db import connection
+    test_utils.setup_test_environment()
+    connection.creation.create_test_db()
+
+    # Imports integration tests
+    from testproj.app.tests import *
+except ImportError:
+    print >> sys.stderr, 'Integration: module "sqlite3" is required... SKIPPED'
--- src/djangoflash/tests/testproj/settings.py
+++ src/djangoflash/tests/testproj/settings.py
@@ -8,9 +8,6 @@ TEMPLATE_DEBUG = DEBUG
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = 'db'
 
-# FLASH_STORAGE = 'session'
-# FLASH_STORAGE = 'cookie'
-
 SECRET_KEY = 'g9b@q$)=^xd2g@-7pg=j=h3*8+xd#hgn-9je@iq5_m#seg&d1y'
 
 MEDIA_URL = '/media/'
