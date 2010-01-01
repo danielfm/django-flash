937f73721c921671b8900561797ec498c0ae24cf
danielfm <daniel.tritone@gmail.com>
2010-01-01 11:20:08 -0200

    Updated year.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Django-Flash'
-copyright = u'2008-2009, Destaquenet Technology Solutions'
+copyright = u'2008-2010, Destaquenet Technology Solutions'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
